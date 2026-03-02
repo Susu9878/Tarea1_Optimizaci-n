348d4fd29b5a32b7aa434b9fe8c6fd75148f8334
Hannia Escamilla Pérez <hanescper@gmail.com>
2026-03-01 20:07:35 -0600

    Added like button

--- src/dynamicList.jsx
+++ src/dynamicList.jsx
@@ -1,12 +1,27 @@
+import { useState } from 'react'
+
+
+function Like(index){
+  const [likes, setLikes] = useState(0)
+  return(
+    <div>
+      <p>Likes: {likes}</p>
+      <button onClick={() => setLikes(likes + 1)}> Dar Like </button>
+    </div>
+  )
+}
+
 function DynamicList() {
   const elementos = ['Elemento 1','Elemento 2','Elemento 3'];
-
+  
   console.log("Render List");
   return (
     <ul>
       {elementos.map((item, index) => (
-    
-        <li key={index}>{item}</li>
+        <div>
+          <li key={index}>{item}</li>
+              <Like></Like>
+        </div>
       ))}
     </ul>
   );
